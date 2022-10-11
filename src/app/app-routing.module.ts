0b4c246f037c0e5882d9e2ff5052335136830e6a
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-11 14:29:26 -0300

    175. Rutas Hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'404',
+    component: ErrorPageComponent
+  },
+  {
+    path:'**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
