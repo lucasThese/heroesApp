f32261847593e0152e34e049dded8d347c20126a
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-12 17:10:29 -0300

    186 Added Basic Navlist

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from "../material/material.module"
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     HeroesRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MaterialModule
 
   ]
 })
