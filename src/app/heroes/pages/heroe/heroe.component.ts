6c770b43cf498b2c4b3ba71a68d758ff9a1221ed
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-13 14:10:56 -0300

    194 Ruta Heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activatedRout: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRout.params
+        .subscribe(({id}) => console.log(id));
   }
 
 }
