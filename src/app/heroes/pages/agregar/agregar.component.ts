882b3836dfd6cc00b872f568fc19fe63cbc5ce05
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-14 14:33:26 -0300

    207. Agregar Heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -8,6 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero:"",
+    alter_ego:"",
+    characters:"",
+    first_appearance:"",
+    publisher: Publisher.DCComics,
+    alt_img: ""
+  }
   constructor() { }
 
   ngOnInit(): void {
