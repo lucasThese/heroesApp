2f91677ee04a7c9819fbac8823135dc78e21fa2e
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-17 11:01:25 -0300

    211. Eliminar registros

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [`
+  img{
+    width: 100%;
+    border-radius: 5px
+  }`
   ]
 })
 export class AgregarComponent implements OnInit {
@@ -29,9 +35,22 @@ export class AgregarComponent implements OnInit {
     publisher: Publisher.DCComics,
     alt_img: ""
   }
-  constructor(private heroesService: HeroesService ) { }
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+
+    if(!this.router.url.includes('editar')){
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroePorId(id))
+      )
+      .subscribe( heroe => this.heroe = heroe )
+
   }
 
   guardar(){
@@ -39,8 +58,14 @@ export class AgregarComponent implements OnInit {
       return;
     }
 
+    if(this.heroe.id){
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(heroe => console.log(heroe))
+    } else {
     this.heroesService.agregarHeroe(this.heroe)
-      .subscribe(resp => {console.log(resp);
+      .subscribe(heroe => {
+        this.router.navigate(['/heroes/editar', heroe.id])
       })
+    }
   }
 }
