79461e1931365dc3951df8cda47bb6a1129e2dc7
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-17 11:56:13 -0300

    212 Borrar completo

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -68,4 +68,13 @@ export class AgregarComponent implements OnInit {
       })
     }
   }
+
+  borrarHeroe(){
+    this.heroesService.borrarHeroe( this.heroe.id! )
+      .subscribe( resp =>{
+        this.router.navigate(['/heroes']);
+      } )
+  }
+
+
 }
--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -12,9 +12,9 @@ export class ImagenPipe implements PipeTransform {
       return 'assets/no-image.png';
     } else if (heroe.alt_img){
       return heroe.alt_img;
+    }else {
+      return `assets/heroes/${heroe.id}.jpg`;
     }
-
-    return `assets/heroes/${heroe.id}.jpg`;
   }
 
 }
