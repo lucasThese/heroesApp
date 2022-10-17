66c82c77ca2c84bbfe7f2d10e9583468951e2312
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-17 15:32:31 -0300

    222. AuthService

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,18 +1,22 @@
 import { Component,  } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginComponent{
+export class LoginComponent {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor( private router: Router) { }
+  login() {
+    this.authService.login().subscribe((resp) => {
+      console.log(resp);
 
-  login(){
-    this.router.navigate(['./heroes']);
+      if (resp.id) {
+        this.router.navigate(['./heroes']);
+      }
+    });
   }
-
 }
