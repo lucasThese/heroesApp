c1259da7351425c8231ea5ae099fded80f12e2bd
Lucas Mesa <lucasmesa2010@hotmail.com>
2022-10-17 14:37:28 -0300

    215 informacion al dialogo

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -80,14 +80,22 @@ export class AgregarComponent implements OnInit {
 
   borrarHeroe(){
 
-    this.dialog.open(ConfirmarComponent,{
-      width: '250px'
+    const dialog = this.dialog.open(ConfirmarComponent,{
+      width: '250px',
+      data: this.heroe
     })
 
-    // this.heroesService.borrarHeroe( this.heroe.id! )
-    //   .subscribe( resp =>{
-    //     this.router.navigate(['/heroes']);
-    //   } )
+    dialog.afterClosed().subscribe(
+      (result) => {
+        if(result){
+          this.heroesService.borrarHeroe( this.heroe.id! )
+          .subscribe( resp =>{
+            this.router.navigate(['/heroes']);
+          } )
+        }
+      }
+    )
+
   }
 
   mostrarSnakbar( mensaje: string){
